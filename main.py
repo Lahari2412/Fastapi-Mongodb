6032048ea1487b69cada48259a859039ae26d041
Lahari2412 <lahari2412@gmail.com>
2024-06-28 20:07:10 +0530

    Changed from index to main

--- main.py
+++ main.py
@@ -24,3 +24,6 @@ app.include_router(user)
 app.include_router(login_router)
 app.include_router(password_reset_router)
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8080)
